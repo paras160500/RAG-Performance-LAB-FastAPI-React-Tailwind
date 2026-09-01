46db281c42a21ff57b8bdf0eb62957cc7844be18
Paras Patel <paraspatel160500@gmail.com>
2026-09-01 17:24:35 +0530

    CORS Update

--- backend/app/main.py
+++ backend/app/main.py
@@ -27,7 +27,7 @@ app.add_middleware(
         allow_origins=[
         "http://localhost:5173",
         "http://127.0.0.1:5173",
-        "https://rag-performance-lab-fastapi-react.onrender.com"
+        "https://rag-performance-lab-fastapi-react-0gdq.onrender.com"
     ],
     allow_credentials=True,
     allow_methods=["*"],
